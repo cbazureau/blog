ffc80efdb199d6f709c056a6d0d30e9f7f832a14
Cédric Bazureau <cedric.bazureau@renault.com>
2022-09-12 15:33:54 +0200

    chore : init blog

---
+++ .github/README.md
@@ -0,0 +1,18 @@
+![](../posts/screenshot.png)
+
+# Cédric's Blog
+
+Deployments in less than 10 seconds.
+
+To get it running:
+
+```sh
+git clone git@github.com:cbazureau/blog.git blog
+cd blog
+deno task dev
+```
+
+To deploy it to Deno Deploy - clone it in a github repo and create and link a
+new project on https://dash.deno.com.
+
+Built using [deno_blog](https://github.com/denoland/deno_blog).
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // import blog, { ga, redirects } from "https://deno.land/x/blog@0.5.0/blog.tsx";
-import blog, { ga, redirects } from "./packages/deno_blog/blog.tsx";
+import blog, { ga, redirects } from "https://cdn.jsdelivr.net/gh/cbazureau/deno_blog@fix/canonical-url/blog.tsx";
 
 blog({
-  title: "Web wandering",
+  title: "Web Wanderings",
   author: "Cédric Bazureau",
   avatar: "./identite.375.jpg",
-  avatarClass: "full",
+  avatarClass: "rounded-full",
   lang: 'fr',
   dateFormat: date => new Intl.DateTimeFormat('fr-FR', { weekday: "long", year: "numeric", month: "long", day: "numeric"}).format(date),
   links: [
